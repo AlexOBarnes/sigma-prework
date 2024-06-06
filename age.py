06f980a70529a4e86f03d2e6b4206ff73b279537
Alex Barnes <a.o.barnes222@gmail.com>
2024-06-06 18:31:51 +0100

    Adding two python files for later work

---
+++ age.py
@@ -0,0 +1,10 @@
+#Create a file called age.py
+#You can run this file by navigating to it through the terminal and
+#typing python age.py
+#Edit this code in VS Code
+#Write a short programme that when given a date calculates the age
+#between the current date and the given date
+#An example input and output might be
+#i. 01-01-1990 should be 34
+#ii. 04-12-1972 should be 51
+#You will likely need to use the python datetime module
---
+++ maxmin.py
@@ -0,0 +1,9 @@
+#Create a file called maxmin.py
+#Run this file by navigating to it in your terminal and typing maxmin.py
+#To edit this file use VS Code
+# given a list of integers return the highest and lowest numbers
+#Without using min() or max()
+#Examples of inputs and outputs are:
+#i.[2,3,1,0,2,-1] should return [-1,4]
+#ii.[20,50,12,6,14,8] should return [6,50]
+#iii.[100,-100] should return [-100,100]
