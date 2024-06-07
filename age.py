bc6cd3bf6040a6a7e26d6ccd090fcdf620cb4fb5
Alex Barnes <a.o.barnes222@gmail.com>
2024-06-07 13:52:31 +0100

    Added created age.py and maxmin.py

--- age.py
+++ age.py
@@ -1,10 +1,17 @@
-#Create a file called age.py
-#You can run this file by navigating to it through the terminal and
-#typing python age.py
-#Edit this code in VS Code
-#Write a short programme that when given a date calculates the age
-#between the current date and the given date
-#An example input and output might be
-#i. 01-01-1990 should be 34
-#ii. 04-12-1972 should be 51
-#You will likely need to use the python datetime module
+from datetime import datetime as dt
+
+def date_converter(date):
+  new_date = dt.strptime(date,"%d-%m-%Y")
+  return new_date
+
+def age_calculator(date):
+  today = dt.today()
+  delta = today - date
+  years = int(delta.days/365.25)
+  print(years)
+  pass
+
+if __name__ == "__main__":
+  date = input("Please enter a date (dd-mm-yyyy): ")
+  new_date = date_converter(date)
+  age_calculator(new_date)
--- maxmin.py
+++ maxmin.py
@@ -1,9 +1,23 @@
-#Create a file called maxmin.py
-#Run this file by navigating to it in your terminal and typing maxmin.py
-#To edit this file use VS Code
-# given a list of integers return the highest and lowest numbers
-#Without using min() or max()
-#Examples of inputs and outputs are:
-#i.[2,3,1,0,2,-1] should return [-1,4]
-#ii.[20,50,12,6,14,8] should return [6,50]
-#iii.[100,-100] should return [-100,100]
+def maxmin(num_list):
+  sorted_num_list = sorted(num_list)
+  minmax = [sorted_num_list[0],sorted_num_list[-1]]
+  return minmax
+
+def alt_maxmin(num_list):
+  min = num_list[0]
+  max = num_list[0]
+  for num in num_list:
+    if num < min:
+      min = num
+    elif num > max:
+      max = num
+  alt_maxmin = [min,max]
+  return alt_maxmin
+
+if __name__ == "__main__":
+  print("Welcome! This program will tell you the highest number in an inputted array")
+  num_list= input("Please enter a list of numbers separated by commas: ")
+  joined_list = num_list.split(",")
+  int_list = [eval(num) for num in joined_list]
+  print(maxmin(int_list))
+  print(alt_maxmin(int_list))
