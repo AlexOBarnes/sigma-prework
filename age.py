8b3d162422b590f2788d02b69300e95cbee87cc3
Alex Barnes <a.o.barnes222@gmail.com>
2024-06-07 13:57:43 +0100

    Add comments to the code

--- age.py
+++ age.py
@@ -1,9 +1,12 @@
 from datetime import datetime as dt
 
+#This function takes the date inputted by the user and converts it
+#To a datetime library compatible date
 def date_converter(date):
   new_date = dt.strptime(date,"%d-%m-%Y")
   return new_date
 
+#This function calculates the age of the person using datetime.timedelta
 def age_calculator(date):
   today = dt.today()
   delta = today - date
@@ -11,6 +14,7 @@ def age_calculator(date):
   print(years)
   pass
 
+#Main block of code that calls the functions to be used
 if __name__ == "__main__":
   date = input("Please enter a date (dd-mm-yyyy): ")
   new_date = date_converter(date)
--- maxmin.py
+++ maxmin.py
@@ -1,8 +1,12 @@
+#This function uses the sorted function on an inputted list of numbers
+#Then returns a list of the largest and smallest numbers
 def maxmin(num_list):
   sorted_num_list = sorted(num_list)
   minmax = [sorted_num_list[0],sorted_num_list[-1]]
   return minmax
 
+#This function also returns a list of the largest and smallest numbers
+#of a given list but does not use built in functions
 def alt_maxmin(num_list):
   min = num_list[0]
   max = num_list[0]
@@ -14,6 +18,8 @@ def alt_maxmin(num_list):
   alt_maxmin = [min,max]
   return alt_maxmin
 
+#Main body of code that takes the users input and creates a list of 
+#integers
 if __name__ == "__main__":
   print("Welcome! This program will tell you the highest number in an inputted array")
   num_list= input("Please enter a list of numbers separated by commas: ")
